e5e672f250180db7046728680eeef0f488a76cda
Ryan Hanks <ryan.hanks@webfilings.com>
2013-01-10 20:41:32 -0600

    add IdeaPlugin

---
+++ src/test/groovy/org/gradlefx/plugins/ide/idea/IdeaPluginTest.groovy
@@ -0,0 +1,174 @@
+package org.gradlefx.plugins.ide.idea
+
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.internal.project.DefaultProject
+import org.gradle.api.tasks.Delete
+import org.gradle.plugins.ide.idea.GenerateIdeaModule
+import org.gradle.plugins.ide.idea.GenerateIdeaProject
+import org.gradle.plugins.ide.idea.GenerateIdeaWorkspace
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradlefx.util.TemporaryFolder
+import spock.lang.Specification
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ryanhanks
+ * Date: 1/10/13
+ * Time: 5:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class IdeaPluginTest extends Specification {
+
+
+    private final DefaultProject project = Helper.createRootProject();
+
+    private final Project childProject = Helper.createChildProject(project, "child", new File("."))
+
+    def "adds 'ideaProject' task to root project"() {
+        when:
+        applyPluginToProjects()
+
+        println project
+        then:
+        assertThatCleanIdeaDependsOnDeleteTask(project, project.cleanIdeaProject)
+        GenerateIdeaProject ideaProjectTask = project.ideaProject
+        ideaProjectTask instanceof GenerateIdeaProject
+        ideaProjectTask.outputFile == new File(project.projectDir, project.name + ".ipr")
+        ideaProjectTask.ideaProject.modules == [project.idea.module, childProject.idea.module]
+        ideaProjectTask.ideaProject.jdkName == JavaVersion.current().toString()
+        ideaProjectTask.ideaProject.languageLevel.level == "JDK_1_6"
+
+        childProject.tasks.findByName('ideaProject') == null
+        childProject.tasks.findByName('cleanIdeaProject') == null
+    }
+
+    def "configures idea project"() {
+        when:
+        applyPluginToProjects()
+
+        then:
+        project.idea.project.wildcards == ['!?*.java', '!?*.groovy'] as Set
+    }
+
+    def "adds 'ideaWorkspace' task to root project"() {
+        when:
+        applyPluginToProjects()
+
+        then:
+        project.ideaWorkspace instanceof GenerateIdeaWorkspace
+        assert project.cleanIdeaWorkspace instanceof Delete
+        assert !project.cleanIdea.taskDependencies.getDependencies(project.cleanIdea).contains(project.cleanIdeaWorkspace)
+
+
+        childProject.tasks.findByName('ideaWorkspace') == null
+        childProject.tasks.findByName('cleanIdeaWorkspace') == null
+    }
+
+    def "adds 'ideaModule' task to projects"() {
+        when:
+        applyPluginToProjects()
+
+        then:
+        assertThatIdeaModuleIsProperlyConfigured(project)
+        assertThatIdeaModuleIsProperlyConfigured(childProject)
+    }
+
+    def "adds special configuration if Java plugin is applied"() {
+        when:
+        applyPluginToProjects()
+        project.apply(plugin: 'java')
+
+        then:
+        project.idea.project.languageLevel.level == new IdeaLanguageLevel(project.sourceCompatibility).level
+
+        def configurations = project.configurations
+        project.idea.module.scopes == [
+                COMPILE: [plus: [configurations.compile], minus: []],
+                RUNTIME: [plus: [configurations.runtime], minus: [configurations.compile]],
+                TEST: [plus: [configurations.testRuntime], minus: [configurations.runtime]],
+                PROVIDED: [plus: [], minus: []]
+        ]
+    }
+
+    def "picks up late changes to build dir"() {
+        when:
+        applyPluginToProjects()
+        project.apply(plugin: 'java')
+        project.buildDir = project.file('target')
+
+        then:
+        project.idea.module.excludeDirs == [project.buildDir, project.file('.gradle')] as Set
+    }
+
+    def "adds 'cleanIdea' task to projects"() {
+        when:
+        applyPluginToProjects()
+
+        then:
+        project.cleanIdea instanceof Task
+        childProject.cleanIdea instanceof Task
+    }
+
+    def "adds single entry libraries from source sets"() {
+        when:
+        applyPluginToProjects()
+        project.apply(plugin: 'java')
+
+        project.sourceSets.main.output.dir 'generated-folder'
+        project.sourceSets.main.output.dir 'ws-generated'
+
+        project.sourceSets.test.output.dir 'generated-test'
+        project.sourceSets.test.output.dir 'test-resources'
+
+        then:
+        def runtime = project.ideaModule.module.singleEntryLibraries.RUNTIME
+        runtime.any { it.name.contains('generated-folder') }
+        runtime.any { it.name.contains('ws-generated') }
+
+        def test = project.ideaModule.module.singleEntryLibraries.TEST
+        test.any { it.name.contains('generated-test') }
+        test.any { it.name.contains('test-resources') }
+    }
+
+    private void assertThatIdeaModuleIsProperlyConfigured(Project project) {
+        GenerateIdeaModule ideaModuleTask = project.ideaModule
+        assert ideaModuleTask instanceof GenerateIdeaModule
+        assert ideaModuleTask.outputFile == new File(project.projectDir, project.name + ".iml")
+        assertThatCleanIdeaDependsOnDeleteTask(project, project.cleanIdeaModule)
+    }
+
+    private void assertThatCleanIdeaDependsOnDeleteTask(Project project, Task dependsOnTask) {
+        assert dependsOnTask instanceof Delete
+        assert project.cleanIdea.taskDependencies.getDependencies(project.cleanIdea).contains(dependsOnTask)
+    }
+
+    private applyPluginToProjects() {
+        project.apply plugin: org.gradlefx.plugins.ide.idea.IdeaPlugin
+        childProject.apply plugin: org.gradlefx.plugins.ide.idea.IdeaPlugin
+    }
+}
+
+class Helper {
+    static DefaultProject createRootProject() {
+        createRootProject(TemporaryFolder.newInstance().dir)
+    }
+
+    static DefaultProject createRootProject(File rootDir) {
+        return ProjectBuilder
+                .builder()
+                .withProjectDir(rootDir)
+                .build()
+    }
+
+    static DefaultProject createChildProject(DefaultProject parent, String name, File projectDir = null) {
+        return ProjectBuilder
+                .builder()
+                .withName(name)
+                .withParent(parent)
+                .withProjectDir(projectDir)
+                .build();
+    }
+}
